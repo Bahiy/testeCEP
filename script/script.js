77f5dca6e5fcb6e8897b6ea70ac1fe493f468772
Jhow Ruan <cesaraugustos0206@gmail.com>
2022-10-10 11:25:09 -0300

    alterações no projeto - mascara cep e limpa formulário

--- script/script.js
+++ script/script.js
@@ -1,29 +1,46 @@
 function consultaEndereco() {
-    let cep = document.querySelector('#cep').value;
-
+    let valorCep = document.querySelector('#cep').value;
+    let cep = valorCep.replace(/\D/g, '');
     if (cep.length != 8) {
         alert("CEP inválido!");
         return;
     }
-
-
     let url = `https://viacep.com.br/ws/${cep}/json/`
-
-    fetch(url).then(function (response) {
+    fetch(url).then((response) => {
         response.json().then(function (data) {
             console.log(data);
             mostraResultado(data)
         })
+    }).catch((err) => {
+        console.log("Erro")
     })
 }
-
 function mostraResultado(dados) {
     let resultado = document.querySelector("#resultado");
     if (resultado)
-    resultado.innerHTML = `<center>
+        resultado.innerHTML = `<center>
                             <p>Cidade: ${dados.localidade} - ${dados.uf}<p>
                             <p>Bairro: ${dados.bairro}
                             <p>Endereço: ${dados.logradouro}<p>
                             <p>Complemento: ${dados.complemento}<p>
+                            <p>DD: ${dados.ddd}<p>
                             <center>`
 }
+
+function limpaDados() {
+    let limpaDivResultado = `
+    <p>Seu resultado Aqui: </p>
+    `;
+    document.querySelector("#resultado").innerHTML = limpaDivResultado;
+}
+
+const cepMask = () => {
+    let cep = document.querySelector('#cep').value;
+    if (cep.length < 5) {
+    } else {
+        document.getElementById('cep').value = cep.substring(0, 5)
+            + "-"
+            + cep.substring(6);
+    }
+}
+
